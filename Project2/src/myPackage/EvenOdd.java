43459315437e2f96778412fc05732324f557a9a9
Manvendra Singh <manvendra1097@gmail.com>
2022-02-14 00:24:31 +0530

    Projct2 added find Even and odd program

---
+++ Project2/src/myPackage/EvenOdd.java
@@ -0,0 +1,21 @@
+package myPackage;
+
+import java.util.Scanner;
+
+public class EvenOdd 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any positive integer");
+		int num = sc.nextInt();
+		
+		if((num|1)== num+1)
+			System.out.println("It's Even");
+		else
+			System.out.println("It's Odd");
+     sc.close();
+	}
+
+}
