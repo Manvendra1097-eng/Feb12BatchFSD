508ab9c4c8ce6a07ff3edef826723d55fa0302fb
Manvendra Singh <manvendra1097@gmail.com>
2022-02-14 00:03:32 +0530

    Project1 added

---
+++ Project1/src/mypackage/Hello.java
@@ -0,0 +1,13 @@
+package mypackage;
+/* Program to print hello 5 time*/
+
+public class Hello 
+{
+
+	public static void main(String[] args) 
+	{
+		for(int i = 0;i<5;i++)
+			System.out.println("Hello");
+	}
+
+}
